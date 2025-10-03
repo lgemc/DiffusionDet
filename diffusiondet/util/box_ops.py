fix: fix the loss calculation

diffusiondet/util/box_ops.py
@@ -47,9 +47,15 @@ def generalized_box_iou(boxes1, boxes2):
     and M = len(boxes2)
     """
     # degenerate boxes gives inf / nan results
-    # so do an early check
-    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
-    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
+    # so fix boxes to ensure x2 >= x1 and y2 >= y1
+    boxes1 = torch.cat([
+        torch.min(boxes1[:, :2], boxes1[:, 2:]),
+        torch.max(boxes1[:, :2], boxes1[:, 2:])
+    ], dim=-1)
+    boxes2 = torch.cat([
+        torch.min(boxes2[:, :2], boxes2[:, 2:]),
+        torch.max(boxes2[:, :2], boxes2[:, 2:])
+    ], dim=-1)
     iou, union = box_iou(boxes1, boxes2)
 
     lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])